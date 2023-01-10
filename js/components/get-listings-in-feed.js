2d51f0617ed4d53bda861a833e0f623322afd14b
ClaesF0 <claes_folkestad@hotmail.com>
2023-01-10 15:21:42 +0100

    implementing filters on frontpage

--- js/components/get-listings-in-feed.js
+++ js/components/get-listings-in-feed.js
@@ -1,10 +1,102 @@
-import { doc } from 'prettier';
+import { check, doc } from 'prettier';
 
 const options = { method: 'GET' };
 
 import moment from "moment/moment";
 //let now = moment(new Date()); //todays date
-let apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?_active=true';
+
+//pagination start
+const pagination = document.getElementById('pagination');
+const pageLinks = pagination.getElementsByTagName('a');
+
+for (let i = 0; i < pageLinks.length; i++) {
+  pageLinks[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    const page = this.innerHTML;
+    console.log(`Navigating to page ${page}`);
+  });
+}
+//pagination end
+
+
+
+
+
+
+
+
+let apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings';
+//urls should consist of endpoint+active state+number of results
+
+
+//construct URL start 
+
+//start of selecting endpoint
+const selectFilterElement = document.getElementById('selectFilterElement');
+let selectedEndpoint; 
+selectFilterElement.addEventListener('change', async function() {
+  const selectedEndpoint = this.value;
+  if (selectedEndpoint === 'all') {
+    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings';
+    await getAllListings(apiUrl)
+    console.log('apiUrl',apiUrl);
+  } else { 
+    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings'+`${selectedEndpoint}`;
+    console.log('apiUrl endpoint inside function',apiUrl);
+    console.log('selectedEndpoint inside function',selectedEndpoint);
+    return selectedEndpoint
+  }
+});
+console.log('selectedEndpoint after function',selectedEndpoint);
+//end of selecting endpoint  
+
+//results per page start
+const resultsSelectorForm = document.getElementById('results-per-page-form');
+const resultsSelector = document.getElementById('results-per-page');
+const resultsPerPage = resultsSelector.value;
+const limit = `?limit=${resultsPerPage}`;
+console.log('limit',limit);
+
+resultsSelector.addEventListener('change', async function () {
+  console.log('resultsPerPage',resultsPerPage);
+  
+})
+
+//results per page end
+let activeState = "";
+const checkbox = document.getElementById('activeOnly');
+function updateURL() {
+  if (checkbox.checked) {
+    activeState += '?_active=true';
+  } else {
+    activeState = activeState.replace("?_active=true", "");
+  }
+  console.log("activeState inside checkbox",activeState);
+}
+checkbox.addEventListener('change', updateURL);
+
+console.log('apiUrl AFTER CHECKBOX',apiUrl);
+
+  //make a loop to construct it as it happens 
+
+
+let baseUrl = selectedEndpoint;
+let variables = [selectedEndpoint, activeState, limit];
+
+for (let i = 0; i < variables.length; i++) {
+baseUrl += selectedEndpoint, activeState, limit///*'?' +*/ variables[i] /*+ '='*/ //+ values[i];
+console.log('baseUrl inside loop', baseUrl);
+
+}
+
+console.log("baseUrl",baseUrl); 
+
+//end of loop 
+
+
+
+
+
 async function getAllListings(URL) {
   
   const response = await fetch(URL,options,);
@@ -81,64 +173,14 @@ getAllListings(apiUrl)
 
 
 
-const selectElement = document.getElementById('selectElement');
 
-selectElement.addEventListener('change', async function() {
-  const selectedValue = this.value;
-
-  if (selectedValue === 'all') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  } else if (selectedValue === 'active') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?_active=true';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  } else if (selectedValue === 'ascendingTitle') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?sort=title&sortOrder=asc';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  } else if (selectedValue === 'descendingTitle') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?sort=title&sortOrder=desc';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  } else if (selectedValue === 'ascendingCreated') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?sort=created&sortOrder=asc';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  } else if (selectedValue === 'descendingCreated') {
-    apiUrl = 'https://nf-api.onrender.com/api/v1/auction/listings?sort=created&sortOrder=desc';
-    await getAllListings(apiUrl)
-    console.log('apiUrl',apiUrl);
-  }
-
-
-
-  const options = { method: 'GET' };
+  //const options = { method: 'GET' };
   const responseTWO = await fetch(apiUrl, options);
   console.log('apiUrl ETTER FETCH 2',apiUrl);
-  console.log('dette er responseTWO RETT ETTER FETCH',responseTWO);
+  //console.log('dette er responseTWO RETT ETTER FETCH',responseTWO);
   
-  const checkbox = document.getElementById('activeOnly');
-  function updateURL() {
-    if (checkbox.checked) {
-      apiUrl += '?_active=true';
-      location.reload();
-    } else {
-      apiUrl = apiUrl.replace("?_active=true", "");
-      location.reload();
-    }
-    console.log("apiUrl CHECKBOX",apiUrl);
-  }
-  checkbox.addEventListener('change', updateURL);
-
-  console.log('apiUrl ETTER FETCH 2 ETTER CHECKBOX',apiUrl);
-  console.log('dette er responseTWO RETT ETTER FETCH ETTER CHECKBOX',responseTWO);
-  
-  
-console.log('console log etter second call');
 if (responseTWO.ok) {
-  console.log("second response RECEIVED")
+  //console.log("second response RECEIVED")
   cardsContainer.innerHTML = "";
 }
 
@@ -200,7 +242,7 @@ const items = responseTWO.json()
   .catch((err) => console.error(err));
   // process the response here
   
-});
+
 
 
 
